c03e3f9bdd8bcf5d04711ac86eb0bbfa91867db2
Atul Tiwari <tiwariatul9526@gmail.com>
2022-07-31 18:43:42 +0530

    handling signin function

---
+++ lib/helper/helper_functions.helper.dart
@@ -0,0 +1,18 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HelperFunctions {
+  //key
+  static String userLoggedInKey = "LOGGEDINKEY";
+  static String userNameKey = "USERNAMEKEY";
+  static String userEmailKey = "USEREMAILKEY";
+
+  //saving the data to shared pref
+
+  //getting the data from shared pref
+
+  static Future<bool?> getUserLoggedInStatus() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    sharedPreferences.getBool(userLoggedInKey);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:groupchatapp/helper/helper_functions.helper.dart';
 import 'package:groupchatapp/shared/constants.shared.dart';
 import 'package:groupchatapp/view/home.view.dart';
+import 'package:groupchatapp/view/login_view.view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,10 +27,32 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSignedIn = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,7 +61,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeView(),
+      home: _isSignedIn ? const HomeView() : const LoginView(),
     );
   }
 }
