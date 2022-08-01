1dc9ade45a8ba84d203a0482819bf65015549702
Atul Tiwari <tiwariatul9526@gmail.com>
2022-08-01 23:59:56 +0530

    create group logic && save the data to firebase

--- lib/view/home.view.dart
+++ lib/view/home.view.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:groupchatapp/helper/helper_functions.helper.dart';
 import 'package:groupchatapp/services/auth_service.services.dart';
+import 'package:groupchatapp/services/database_service.services.dart';
 import 'package:groupchatapp/shared/styles.shared.dart';
 import 'package:groupchatapp/view/auth/login_view.view.dart';
 import 'package:groupchatapp/view/profile_view.view.dart';
@@ -18,6 +20,9 @@ class _HomeViewState extends State<HomeView> {
   String userName = "";
   String email = "";
   AuthServices authServices = AuthServices();
+  Stream? groups;
+  bool _isLoading = false;
+  String groupName = "";
 
   @override
   void initState() {
@@ -37,99 +42,22 @@ class _HomeViewState extends State<HomeView> {
         email = value!;
       });
     });
+
+    //getting the list of snapshot in our stream
+    await DatabaseServices(uid: FirebaseAuth.instance.currentUser!.uid)
+        .getUserGroups()
+        .then((snapshots) {
+      setState(() {
+        groups = snapshots;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-        elevation: 1.0,
-        title: Text(
-          "Groups",
-          style: headingText.copyWith(
-              fontSize: 24, color: Colors.white, fontWeight: FontWeight.w300),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () => nextScreen(context, const SearchView()),
-            icon: const Icon(Icons.search_outlined),
-          )
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 50),
-          children: [
-            const Icon(Icons.account_circle, size: 150),
-            const SizedBox(height: 10),
-            Text(
-              userName,
-              textAlign: TextAlign.center,
-              style: lightText.copyWith(
-                  fontWeight: FontWeight.w500, color: Colors.black),
-            ),
-            const SizedBox(height: 30),
-            const Divider(height: 2),
-            ListTile(
-              onTap: () {},
-              selectedColor: Theme.of(context).primaryColor,
-              selected: true,
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-              leading: const Icon(Icons.groups_rounded),
-              title:
-                  const Text("Groups", style: TextStyle(color: Colors.black)),
-            ),
-            ListTile(
-              onTap: () => nextScreen(
-                  context, ProfileView(userName: userName, email: email)),
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-              leading: const Icon(Icons.person_rounded),
-              title:
-                  const Text("Profile", style: TextStyle(color: Colors.black)),
-            ),
-            ListTile(
-              onTap: () async {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text("Logout"),
-                      content: Text("Are you sure you want to logout"),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text(
-                            "No",
-                            style: TextStyle(color: Colors.red),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            authServices.signOut().whenComplete(() {
-                              nextScreenReplacement(context, const LoginView());
-                            });
-                          },
-                          child: const Text("Yes",
-                              style: TextStyle(color: Colors.green)),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-              leading: const Icon(Icons.logout_rounded),
-              title:
-                  const Text("Logout", style: TextStyle(color: Colors.black)),
-            ),
-          ],
-        ),
-      ),
+      appBar: buildAppBar(context),
+      drawer: buildDrawer(context),
       body: groupList(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
@@ -142,7 +70,238 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
-  popUpDialog(BuildContext context) {}
+  Drawer buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 50),
+        children: [
+          const Icon(Icons.account_circle, size: 150),
+          const SizedBox(height: 10),
+          Text(
+            userName,
+            textAlign: TextAlign.center,
+            style: lightText.copyWith(
+                fontWeight: FontWeight.w500, color: Colors.black),
+          ),
+          const SizedBox(height: 30),
+          const Divider(height: 2),
+          ListTile(
+            onTap: () {},
+            selectedColor: Theme.of(context).primaryColor,
+            selected: true,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+            leading: const Icon(Icons.groups_rounded),
+            title: const Text("Groups", style: TextStyle(color: Colors.black)),
+          ),
+          ListTile(
+            onTap: () => nextScreen(
+                context, ProfileView(userName: userName, email: email)),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+            leading: const Icon(Icons.person_rounded),
+            title: const Text("Profile", style: TextStyle(color: Colors.black)),
+          ),
+          ListTile(
+            onTap: () async {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Logout"),
+                    content: const Text("Are you sure you want to logout"),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text(
+                          "No",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          authServices.signOut().whenComplete(() {
+                            nextScreenReplacement(context, const LoginView());
+                          });
+                        },
+                        child: const Text("Yes",
+                            style: TextStyle(color: Colors.green)),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+            leading: const Icon(Icons.logout_rounded),
+            title: const Text("Logout", style: TextStyle(color: Colors.black)),
+          ),
+        ],
+      ),
+    );
+  }
 
-  groupList() {}
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.purple,
+      elevation: 1.0,
+      title: Text(
+        "Groups",
+        style: headingText.copyWith(
+            fontSize: 24, color: Colors.white, fontWeight: FontWeight.w300),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () => nextScreen(context, const SearchView()),
+          icon: const Icon(Icons.search_outlined),
+        )
+      ],
+    );
+  }
+
+  popUpDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Create a Group", textAlign: TextAlign.left),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _isLoading == true
+                    ? Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          color: Colors.red,
+                        ),
+                      )
+                    : TextField(
+                        onChanged: (newValue) {
+                          setState(() {
+                            groupName = newValue;
+                          });
+                        },
+                        style: const TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor),
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                      ),
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  elevation: 0,
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text("CANCEL"),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  primary: Colors.green,
+                ),
+                onPressed: () async {
+                  //
+                  if (groupName != "") {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                    DatabaseServices(
+                            uid: FirebaseAuth.instance.currentUser!.uid)
+                        .createGroup(userName,
+                            FirebaseAuth.instance.currentUser!.uid, groupName)
+                        .whenComplete(() {
+                      setState(() {
+                        _isLoading = false;
+                      });
+                      Navigator.of(context).pop();
+                      //show message
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                            "Group Created Successfully",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                    });
+                  }
+                },
+                child: const Text("CREATE"),
+              ),
+            ],
+          );
+        });
+  }
+
+  groupList() {
+    return StreamBuilder(
+      stream: groups,
+      builder: (context, AsyncSnapshot snapshot) {
+        //make some checks
+        if (snapshot.hasData) {
+          if (snapshot.data["groups"] != null) {
+            if (snapshot.data["groups"].length != 0) {
+              return Text("data");
+            } else {
+              return noGroupWidget();
+            }
+          } else {
+            return noGroupWidget();
+          }
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Theme.of(context).primaryColor,
+              color: Colors.red,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  noGroupWidget() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () => popUpDialog(context),
+            child: Icon(
+              Icons.add_circle,
+              color: Colors.grey[700],
+              size: 75,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+              "You've not joined any groups , tap on the add icon to create a group or also search from top search ",
+              textAlign: TextAlign.center),
+        ],
+      ),
+    );
+  }
 }
