6e5538aa677f45f4941a6414a74ffacacc95ab8b
Atul Tiwari <tiwariatul9526@gmail.com>
2022-08-02 21:41:05 +0530

    fetching chat data

--- lib/view/chat_view.view.dart
+++ lib/view/chat_view.view.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:groupchatapp/services/database_service.services.dart';
 import 'package:groupchatapp/view/group_info.view.dart';
+import 'package:groupchatapp/widgets/message_tile.widgets.dart';
 import 'package:groupchatapp/widgets/widgets.dart';
 
 class ChatView extends StatefulWidget {
@@ -21,6 +22,7 @@ class ChatView extends StatefulWidget {
 class _ChatViewState extends State<ChatView> {
   Stream<QuerySnapshot>? chats;
   String admin = "";
+  TextEditingController _messageController = TextEditingController();
 
   @override
   void initState() {
@@ -66,6 +68,88 @@ class _ChatViewState extends State<ChatView> {
               icon: const Icon(Icons.info_outline)),
         ],
       ),
+      body: Stack(
+        children: [
+          //chat messages
+          chatMessages(),
+          Container(
+            alignment: Alignment.bottomCenter,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _messageController,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                          hintText: "Send a message...",
+                          hintStyle:
+                              TextStyle(color: Colors.white, fontSize: 16)),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  GestureDetector(
+                    onTap: () {
+                      sendMessage();
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Icon(Icons.send_outlined,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  chatMessages() {
+    return StreamBuilder(
+        stream: chats,
+        builder: (context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    return MessageTileWidget(
+                      message: snapshot.data.docs[index]["message"],
+                      sender: snapshot.data.docs[index]["sender"],
+                      sentByMe: widget.userName ==
+                          snapshot.data.docs[index]["sender"],
+                    );
+                  },
+                )
+              : Container();
+        });
+  }
+
+  sendMessage() {
+    if (_messageController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "message": _messageController.text,
+        "sender": widget.userName,
+        "time": DateTime.now().millisecondsSinceEpoch,
+      };
+
+      DatabaseServices().sendMessage(widget.groupId, chatMessageMap);
+      setState(() {
+        _messageController.clear();
+      });
+    }
   }
 }
